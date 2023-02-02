8c9ff725d43417efc0c56f8183dcbba8945759b4
cslucasfernandes <cs.lucasfernandes@gmail.com>
2023-02-02 16:35:48 -0300

    Variables

---
+++ variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int integer_number;
+    printf("Enter a integer number: ");
+    scanf("%d", &integer_number);
+    printf("Your number is: %d\n", integer_number);
+    return 0;
+}
+
